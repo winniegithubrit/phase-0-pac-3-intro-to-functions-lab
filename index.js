569eba9b50fd0f8619da5eefec6996866c5f3dbf
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-09 19:09:05 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function shout(string) { 
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase()==string) {
+    return "I can't hear you!";
+  }else{
+    return "Hi there";
+  }
+}
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase() === "Let's have dinner together!") {
+  return "I would love to!";
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  else {
+    return "YES INDEED!";
+  }
+}
+function sayHiToHeadphonedRoommate(string) {
+  if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  }
+  else if (string.toLowerCase() ===string) {
+    return "I can't hear you!";
+  }
+  else{
+    return "YES INDEED!";
+  }
+
+  
+}
